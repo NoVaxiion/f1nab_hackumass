d8fd54d19196060b0dd96c8fb3ff22f5f9c9c6b8
Sergio <smontufar250@gmail.com>
2024-11-09 12:54:26 -0500

    fixed box sizing and added url for embedding

--- f1_front_end/src/App.js
+++ f1_front_end/src/App.js
@@ -15,16 +15,19 @@ import { useState } from 'react';
 function App() {
   return (
     <div>
-      // Box for Embedding
+      {/* Box for Embedding*/}
       <div id="embedding-box">
         <iframe 
-        src="input link here" 
-        title="embedded video"
+        width="800px"
+        height="600px"
+        src="https://f1tv.formula1.com/search?igu=1" // Replace with your video URL
+        title="F1 TV Embedding"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
         ></iframe>
       </div>
 
-      // Box for the Chat AI
+      {/* Box for the Chat AI */}
       <div id="aichat-box">
         <div id="input">
           <p><strong>User:</strong> Hello there!</p>
