94c55e23428a16995cdfa13951c2229ee565cf70
ryanguyen <rqnguyen@umass.edu>
2024-11-09 23:24:09 -0500

    updated top margin

--- f1_front_end/src/App.js
+++ f1_front_end/src/App.js
@@ -82,7 +82,7 @@ function App() {
         ></iframe>
       </div>
       <div id="aichat-box" style={{ position: 'relative', height: '100vh' }}>
-        <div id="margin-top">f1Nab</div>
+        <div id="margin-top">F1 NAB</div>
         <div id="aichat-box-inner">
           {messages.length === 0 && !messageSent && (
             <div className="input-container">
