5e04cd6f6349dce6488ffd0da98ae7c04863142a
Kenneth Maeda <kenneth.maeda@uconn.edu>
2025-12-02 13:17:47 -0500

    Placeholder

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from query import set_race_context, process_query
+from f1nab_hackumass.query import set_race_context, process_query
 
 app = Flask(__name__)
 CORS(app)
